98997a1b39283f809c81c20aff25da9560143c5e
Andrew Eckhoff <eckhoff@protonmail.com>
2024-12-14 20:26:18 -0600

    first script for quick capture notes from terminal

---
+++ quick-capture.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# File path to Quick-Capture.md
+NOTE_FILE="$HOME/Documents/notes/Notes/Quick-Capture.md"
+
+# Ensure the directory and file exist
+mkdir -p "$(dirname "NOTE_FILE")"
+touch "$NOTE_FILE"
+
+set -f
+
+# if the argument --list is passed print quick-capture.md in terminal
+if [[ "$1" == "--list" ]]; then
+    if [[ ! -s "$NOTE_FILE" ]]; then
+        echo "No notes found in Quick-Capture.md"
+    else
+        echo "Quick Capture Notes:"
+        cat "$NOTE_FILE"
+    fi
+    exit 0
+fi
+
+# Check if text is provided
+if [ -z "$*" ]; then
+    echo "No text provided. Usage: qq <text>"
+    exit 1
+fi
+# old code
+# echo "[$(date '+%Y-%m-%d')] $*" >> "$NOTE_FILE"
+
+# append the input text to the note to Quick-Capture.md
+echo "* $*" >> "$NOTE_FILE"
+echo "Note added: $*"
